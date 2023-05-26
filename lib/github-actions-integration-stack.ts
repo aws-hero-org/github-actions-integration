d14f98fe527a75e884fe22b88fca067a02dfd8a9
Florian Siegel <florian.siegel@netlight.com>
2023-05-26 17:41:34 +0200

    Add bucket for templates to stack

--- lib/github-actions-integration-stack.ts
+++ lib/github-actions-integration-stack.ts
@@ -1,10 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GithubActionsIntegrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const templateBucket = new Bucket(this, 'template-bucket', {})
+
+    new cdk.CfnOutput(this, 'template-bucket-arn', {
+      value: templateBucket.bucketArn
+    })
 
     // The code that defines your stack goes here
 
